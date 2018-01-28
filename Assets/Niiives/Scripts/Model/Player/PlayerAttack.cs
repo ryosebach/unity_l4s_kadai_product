60eae2a9f15d45ebaf8d406d271d480dfe231500
ryosebach <ryo.freezia@gmail.com>
2018-01-29 08:56:39 +0900

    add PlayerAttack.cs

---
+++ Assets/Niiives/Scripts/Model/Player/PlayerAttack.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+namespace Niiives {
+	public class PlayerAttack : MonoBehaviour {
+		void Start() {
+			var input = GetComponent<IPlayerInput>();
+			var shot = GetComponent<IPlayerShot>();
+			input.OnMoveInputObserbable
+				 .Where(_ => !GameManager.Instance.isPause)
+				 .Where(x => x == Const.PlayerInput.Space)
+				 .Subscribe(_ => shot.Shot());
+		}
+	}
+}
