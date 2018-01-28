1b1e677bb2cd5d8d70089ae39ac62c7828aa5bb8
ryosebach <ryo.freezia@gmail.com>
2018-01-29 08:50:30 +0900

    add GameManager

---
+++ Assets/Niiives/Scripts/Manager/GameManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Niiives {
+	public class GameManager : SingletonMonoBehaviour<GameManager> {
+		public bool isPause = false;
+	}
+}
