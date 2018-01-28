0aac2c7a16f448a8f84aa76a375b59a6cb35bcf0
ryosebach <ryo.freezia@gmail.com>
2018-01-29 08:54:31 +0900

    add Player.cs

---
+++ Assets/Niiives/Scripts/Model/Player/Player.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+namespace Niiives {
+	public class Player : SingletonMonoBehaviour<Player> {
+		public FloatReactiveProperty moveSpeed = new FloatReactiveProperty();
+	}
+}
