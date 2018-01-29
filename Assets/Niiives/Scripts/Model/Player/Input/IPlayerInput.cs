918d9857a731b9d35b177a1db07f792a4b601f21
ryosebach <ryo.freezia@gmail.com>
2018-01-29 22:16:42 +0900

    move iplayerinput interface to Input-folder

--- Assets/Niiives/Scripts/Model/Player/Input/IPlayerInput.cs
+++ Assets/Niiives/Scripts/Model/Player/Input/IPlayerInput.cs
@@ -1,11 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UniRx;
+﻿using UniRx;
+
 namespace Niiives {
 
 	public interface IPlayerInput {
 		IObservable<Const.PlayerInput> OnMoveInputObserbable { get; }
 	}
-
 }
