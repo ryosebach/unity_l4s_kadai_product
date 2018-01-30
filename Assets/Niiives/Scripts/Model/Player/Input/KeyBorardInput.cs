a2473df31bc11dc7d3c8bb2dac43432e33584351
ryosebach <ryo.freezia@gmail.com>
2018-01-31 07:22:21 +0900

    add KeyboardInput wasd-input

--- Assets/Niiives/Scripts/Model/Player/Input/KeyBorardInput.cs
+++ Assets/Niiives/Scripts/Model/Player/Input/KeyBorardInput.cs
@@ -23,21 +23,21 @@ namespace Niiives {
 
 		void Start() {
 			this.UpdateAsObservable()
-				.Where(_ => Input.GetKey(KeyCode.DownArrow))
+				.Where(_ => Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
 				.Select(_ => Const.PlayerInput.Down)
 				.Subscribe(onPlyaerInputSubject)
 				.AddTo(this.gameObject);
 			this.UpdateAsObservable()
-				.Where(_ => Input.GetKey(KeyCode.RightArrow))
+				.Where(_ => Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
 				.Select(_ => Const.PlayerInput.Right)
 				.Subscribe(onPlyaerInputSubject)
 				.AddTo(this.gameObject);
 			this.UpdateAsObservable()
-				.Where(_ => Input.GetKey(KeyCode.LeftArrow))
+				.Where(_ => Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
 				.Select(_ => Const.PlayerInput.Left)
 				.Subscribe(onPlyaerInputSubject);
 			this.UpdateAsObservable()
-				.Where(_ => Input.GetKey(KeyCode.UpArrow))
+				.Where(_ => Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
 				.Select(_ => Const.PlayerInput.Up)
 				.Subscribe(onPlyaerInputSubject)
 				.AddTo(this.gameObject);
