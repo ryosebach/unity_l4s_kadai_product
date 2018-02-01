8b2d241027e1f69b1143c5a9a060d88ad11caa82
ryosebach <ryo.freezia@gmail.com>
2018-02-01 09:00:21 +0900

    add player status

--- Assets/Niiives/Scripts/Model/Player/Player.cs
+++ Assets/Niiives/Scripts/Model/Player/Player.cs
@@ -6,5 +6,7 @@ using UniRx;
 namespace Niiives {
 	public class Player : SingletonMonoBehaviour<Player> {
 		public FloatReactiveProperty moveSpeed = new FloatReactiveProperty();
+		public FloatReactiveProperty rollSpeed = new FloatReactiveProperty();
+		public FloatReactiveProperty pitchSpeed = new FloatReactiveProperty();
 	}
 }
