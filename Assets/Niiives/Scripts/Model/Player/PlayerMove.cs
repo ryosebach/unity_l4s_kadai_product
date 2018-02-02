21f42fc387ad7b63f11325efd313673c4068767e
ryosebach <ryo.freezia@gmail.com>
2018-02-02 09:21:22 +0900

    feat : chnage move method

--- Assets/Niiives/Scripts/Model/Player/PlayerMove.cs
+++ Assets/Niiives/Scripts/Model/Player/PlayerMove.cs
@@ -2,36 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UniRx;
+using UniRx.Triggers;
+using UnityStandardAssets.Vehicles.Aeroplane;
 
 namespace Niiives {
 	public class PlayerMove : MonoBehaviour {
 
 		void Start() {
 			var input = GetComponent<IPlayerInput>();
+			var controller = GetComponent<AeroplaneController>();
+			print(controller);
 
+			this.UpdateAsObservable()
+				.Subscribe(_ => controller.Move(0, 0, 0, Player.Instance.moveSpeed.Value, false));
 			input.OnMoveInputObserbable
 				 .Where(_ => !GameManager.Instance.isPause)
 				 .Where(x => x == Const.PlayerInput.Up)
 				 .Subscribe(_ => {
-					 transform.position += Vector3.up * Time.deltaTime * Player.Instance.moveSpeed.Value;
+					 controller.Move(0, Player.Instance.pitchSpeed.Value, 0, 0, false);
+					 //transform.position += Vector3.up * Time.deltaTime * Player.Instance.moveSpeed.Value;
 				 });
 			input.OnMoveInputObserbable
 				 .Where(_ => !GameManager.Instance.isPause)
 				 .Where(x => x == Const.PlayerInput.Right)
 				 .Subscribe(_ => {
-					 transform.position += Vector3.right * Time.deltaTime * Player.Instance.moveSpeed.Value;
+					 controller.Move(Player.Instance.rollSpeed.Value, 0, 0, 0, false);
+					 //transform.position += Vector3.right * Time.deltaTime * Player.Instance.moveSpeed.Value;
 				 });
 			input.OnMoveInputObserbable
 				 .Where(_ => !GameManager.Instance.isPause)
 				 .Where(x => x == Const.PlayerInput.Down)
 				 .Subscribe(_ => {
-					 transform.position += Vector3.down * Time.deltaTime * Player.Instance.moveSpeed.Value;
+					 controller.Move(0, -Player.Instance.pitchSpeed.Value, 0, 0, false);
+					 //transform.position += Vector3.down * Time.deltaTime * Player.Instance.moveSpeed.Value;
 				 });
 			input.OnMoveInputObserbable
 				 .Where(_ => !GameManager.Instance.isPause)
 				 .Where(x => x == Const.PlayerInput.Left)
 				 .Subscribe(_ => {
-					 transform.position += Vector3.left * Time.deltaTime * Player.Instance.moveSpeed.Value;
+					 controller.Move(-Player.Instance.rollSpeed.Value, 0, 0, 0, false);
+					 //transform.position += Vector3.left * Time.deltaTime * Player.Instance.moveSpeed.Value;
 				 });
 		}
 	}
