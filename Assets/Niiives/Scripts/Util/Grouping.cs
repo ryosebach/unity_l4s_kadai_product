1a72e27b72411f50ddac986bbfd0dd996e36c3c2
ryosebach <ryo.freezia@gmail.com>
2018-02-05 08:00:21 +0900

    chore: add grouping.cs

---
+++ Assets/Niiives/Scripts/Util/Grouping.cs
@@ -0,0 +1,28 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public static class Grouping
+{
+    [MenuItem("Edit/Group %g")]
+    private static void Execute()
+    {
+        var gameObjects = Selection.gameObjects;
+
+        if (gameObjects.Length < 0) return;
+
+        var group = new GameObject("Group");
+        Undo.RegisterCreatedObjectUndo(group, "Group");
+        foreach (var go in gameObjects)
+        {
+            Undo.SetTransformParent(go.transform, group.transform, "Group");
+        }
+
+        Selection.activeGameObject = group;
+    }
+
+    [MenuItem("Edit/Group %g", true)]
+    private static bool CanExecute()
+    {
+        return 0 < Selection.transforms.Length;
+    }
+}
