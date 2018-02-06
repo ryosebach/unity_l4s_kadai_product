8fb4a004b85aae1239dc36352a850dfc343584f8
ryosebach <ryo.freezia@gmail.com>
2018-02-06 11:12:33 +0900

    feat: manage search area distance

--- Assets/Niiives/Scripts/Model/Player/Player.cs
+++ Assets/Niiives/Scripts/Model/Player/Player.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UniRx;
+using UniRx.Triggers;
 
 namespace Niiives {
 	public class Player : SingletonMonoBehaviour<Player> {
 		public FloatReactiveProperty moveSpeed = new FloatReactiveProperty();
 		public FloatReactiveProperty rollSpeed = new FloatReactiveProperty();
 		public FloatReactiveProperty pitchSpeed = new FloatReactiveProperty();
+
+		public float targetingDistance = 600;
 	}
 }
