7149573cfac305bfbd9051eb55d4681e710d91ad
ryosebach <ryo.freezia@gmail.com>
2018-02-06 10:00:22 +0900

    feat: add enemyManager

---
+++ Assets/Niiives/Scripts/Manager/EnemyManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using System.Linq;
+
+public class EnemyManager : SingletonMonoBehaviour<EnemyManager> {
+	List<GameObject> enemyObjList = new List<GameObject>();
+	public IObservable<GameObject> OnDestroyedEnemyObservable {
+		get { return destroyedEnemySubject.AsObservable(); }
+	}
+	private Subject<GameObject> destroyedEnemySubject = new Subject<GameObject>();
+
+	private void Start() {
+		enemyObjList = GameObject.FindGameObjectsWithTag("Enemy").ToList();
+	}
+
+	public void DestroyedEnemy(GameObject enemy) {
+		enemyObjList.Remove(enemy);
+		destroyedEnemySubject.OnNext(enemy);
+	}
+
+}
