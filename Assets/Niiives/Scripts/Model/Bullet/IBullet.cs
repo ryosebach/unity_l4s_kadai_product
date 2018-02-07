da5675526f4df3127b0c5a358e88385925b9e273
ryosebach <ryo.freezia@gmail.com>
2018-02-07 22:00:12 +0900

    feat: add bullet interface

--- Assets/Niiives/Scripts/Model/Bullet/IBullet.cs
+++ Assets/Niiives/Scripts/Model/Bullet/IBullet.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using UnityEngine;
 
 namespace Niiives {
 	public interface IBullet {
 		Const.BulletType GetBulletType();
+		float DamegeVal { get; set; }
+		GameObject bulletObj { get; }
 	}
 }
