07f8346ec4c40bd20d9cdb2b6e4c39d86c6fea29
ryosebach <ryo.freezia@gmail.com>
2018-02-09 01:00:14 +0900

    feat: add aim interface

---
+++ Assets/Niiives/Scripts/Model/Player/IPlayerAim.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+namespace Niiives {
+	public interface IPlayerAim {
+		IObservable<GameObject> OnAimedTargetObservable {
+			get;
+
+		}
+	}
+}
