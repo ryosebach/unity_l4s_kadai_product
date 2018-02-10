bbca9c745c440dd3757203959850cd26745e59fd
ryosebach <ryo.freezia@gmail.com>
2018-02-10 23:33:13 +0900

    feat: add EnemyHit

---
+++ Assets/Niiives/Scripts/Model/Enemy/EnemyHit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+namespace Niiives {
+	public class EnemyHit : MonoBehaviour {
+		private void Start() {
+			this.OnTriggerEnterAsObservable()
+				.Do(_ => Debug.Log("hoge"))
+				.Select(x => x.gameObject.GetComponent<IBullet>())
+				.Where(x => x != null)
+				.Subscribe(iBullet => {
+					Debug.Log("hit");
+					var iEnemy = GetComponent<IEnemy>();
+					iEnemy.HP -= iBullet.DamegeVal;
+					Destroy(iBullet.bulletObj);
+				});
+		}
+	}
+}
