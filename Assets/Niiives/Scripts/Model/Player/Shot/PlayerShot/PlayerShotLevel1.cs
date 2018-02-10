c8331d903621dc1c50b6bc684a922ab27a06af92
ryosebach <ryo.freezia@gmail.com>
2018-02-10 22:44:44 +0900

    feat: shot needs iPlayerAim interface and shot form muzzle Position

--- Assets/Niiives/Scripts/Model/Player/Shot/PlayerShot/PlayerShotLevel1.cs
+++ Assets/Niiives/Scripts/Model/Player/Shot/PlayerShot/PlayerShotLevel1.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
 
 namespace Niiives {
 	public class PlayerShotLevel1 : MonoBehaviour, IPlayerShot {
@@ -8,23 +9,37 @@ namespace Niiives {
 		private GameObject bullet1;
 		private const float shotInterval = 0.08f;
 		private float shotTimer;
+		private IPlayerAim iplayerAim;
+		[SerializeField] private Transform muzzleTrans;
 
 		public void Shot() {
 			shotTimer += Time.deltaTime;
 			if (shotTimer > shotInterval) {
-				shotTimer = 0;
-				var bulletObj = Instantiate(bullet1, transform.position, Quaternion.identity) as GameObject;
-				var bullet = bulletObj.GetComponent<Bullet>();
-				bullet.bulletType = Const.BulletType.PlayerBullet;
-				Vector3 shotPos = transform.position;
-				bullet.xMove = (timer) => { return shotPos.x + 3 * Mathf.Sin(timer * 4); };
-				bullet.yMove = (timer) => { return shotPos.y + 3 * Mathf.Cos(timer * 4); };
-				bullet.zMove = (timer) => { return shotPos.z + timer * 8; };
+				iplayerAim.OnAimedTargetObservable
+						  .Subscribe(x => {
+							  shotTimer = 0;
+							  var bulletObj = Instantiate(bullet1, muzzleTrans.position, Quaternion.identity) as GameObject;
+							  var bullet = bulletObj.GetComponent<Bullet>();
+							  bullet.bulletType = Const.BulletType.PlayerBullet;
+							  bullet.DamegeVal = 1.5f;
+							  Vector3 shotPos = transform.position;
+							  Vector3 shotDir;
+							  if (x) {
+								  shotDir = (x.transform.position - shotPos).normalized;
+							  } else {
+								  shotDir = transform.forward;
+							  }
+							  bullet.xMove = (timer) => { return shotPos.x + shotDir.x * timer * 300; };
+							  bullet.yMove = (timer) => { return shotPos.y + shotDir.y * timer * 300; };
+							  bullet.zMove = (timer) => { return shotPos.z + shotDir.z * timer * 300; };
+
+						  });
 			}
 		}
 
 		private void Awake() {
 			bullet1 = Resources.Load("Bullets/Bullet1") as GameObject;
+			iplayerAim = GetComponent<IPlayerAim>();
 		}
 	}
 
