8f92ec6eeed4ec73528f81917da13fb2b7ebe290
ryosebach <ryo.freezia@gmail.com>
2018-02-10 23:12:44 +0900

    feat: implove IEnemy

---
+++ Assets/Niiives/Scripts/Model/Enemy/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+namespace Niiives {
+	public class Enemy : MonoBehaviour, IEnemy {
+		private float hp = 10;
+		public float HP {
+			get { return hp; }
+			set { hp = value; }
+		}
+
+		private void Start() {
+			this.UpdateAsObservable()
+				.Where(_ => HP <= 0)
+				.Subscribe(_ => {
+					Destroy(this.gameObject);
+					EnemyManager.Instance.DestroyedEnemy(this.gameObject);
+				}).AddTo(this.gameObject);
+		}
+	}
+}
